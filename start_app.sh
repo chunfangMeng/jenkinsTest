5609fb0b30c739702a894b320ee3b91b96bc981f
mengchunfang <meng1126441924@gmail.com>
2024-01-18 09:42:37 +0800

    build

--- start_app.sh
+++ start_app.sh
@@ -3,7 +3,7 @@
 python manage.py collectstatic --noinput&&
 python manage.py makemigrations&&
 python manage.py migrate &&
-gunicorn -c gunicorn.conf.py jenkinsTest.wsgi &&
+gunicorn -c ./gunicorn.conf.py jenkinsTest.wsgi &&
 tail -f /dev/null
 
 exec "$@"
