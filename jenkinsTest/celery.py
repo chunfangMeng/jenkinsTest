1e6a6788dd93a88aaed9a40a44db605f36a893aa
mengchunfang <meng1126441924@gmail.com>
2024-01-21 16:38:45 +0800

    build

---
+++ jenkinsTest/celery.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import os
+
+from __future__ import absolute_import
+from celery import Celery, platforms
+from dotenv import load_dotenv
+
+platforms.C_FORCE_ROOT = True
+
+load_dotenv()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.settings')
+
+
+app = Celery(
+    'app_celery',
+    backend=os.getenv('CELERY_BACKEND'),
+    broker=os.getenv('CELERY_BROKER'),
+    include=[
+        'auth_app.celery_tasks',
+    ])
+
+app.config_from_object('django.conf:settings')
+
+app.conf.update(
+    worker_pool_restart=True,
+    worker_prefetch_multiplier=1,
+    enable_utc=True,
+    timezone='Europe/London',
+    CELERYBEAT_SCHEDULE={
+
+    }
+)
+
+app.autodiscover_tasks()
